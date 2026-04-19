fc374683615cb2e66dc9a642886f2ddfb7b66a3b
Bringas Gonzalo Matias <gonzalo.bringas@alu.frt.utn.edu.ar>
2026-04-19 02:31:33 -0300

    ventana y tabla de alta de vehiculos

--- src/main/java/data/Persistencia.java
+++ src/main/java/data/Persistencia.java
@@ -10,22 +10,22 @@ public class Persistencia {
     private static ArrayList<Sucursal> sucursales = new ArrayList<>();
     
     private static void inicializarResponsables(){
-        Responsable r1 = new Responsable("Carlos Gómez", "25444111", "3815551111");
+        /*Responsable r1 = new Responsable("Carlos Gómez", "25444111", "3815551111");
         Responsable r2 = new Responsable("Laura Pérez", "30111222", "3815552222");
         responsables.add(r1);
-        responsables.add(r2);
+        responsables.add(r2);*/
     }
     
     private static void inicializarSucursales(){
-        Sucursal s1 = new Sucursal("SUC01", "Av. Belgrano 1200", "Tucumán", responsables.get(0));
+        /*Sucursal s1 = new Sucursal("SUC01", "Av. Belgrano 1200", "Tucumán", responsables.get(0));
         Sucursal s2 = new Sucursal("SUC02", "San Martín 450", "Yerba Buena", responsables.get(1));
         
         sucursales.add(s1);
-        sucursales.add(s2);
+        sucursales.add(s2);*/
     }
     
     private static void inicializarVehiculos(){
-        Sucursal s1 = sucursales.get(0);
+        /*Sucursal s1 = sucursales.get(0);
         Sucursal s2 = sucursales.get(1);
         
         VehiculoElectrico v1 = new VehiculoElectrico("AE123FG", "Renault", "Kangoo E-Tech", 2020, 1000, s1, 16);
@@ -37,7 +37,7 @@ public class Persistencia {
         vehiculos.add(v1);
         vehiculos.add(v2);
         vehiculos.add(v3);
-        vehiculos.add(v4);
+        vehiculos.add(v4);*/
     }
     
     public static ArrayList<Vehiculo> getVehiculos(){
@@ -45,9 +45,12 @@ public class Persistencia {
     }
     
     public static Optional<Vehiculo> getVehiculo(String patente){
-        return vehiculos.stream()
+        /*return vehiculos.stream()
                 .filter(v -> v.getPatente().equals(patente))
-                .findFirst();
+                .findFirst();*/return null;
+        /*return vehiculos.stream()
+                .filter(v -> v.getPatente().equals(patente))
+                .findFirst();*/
     }
     
     public static void inicializar(){
